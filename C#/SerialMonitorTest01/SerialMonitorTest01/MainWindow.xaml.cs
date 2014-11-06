34c15068149b628a4b5760fc5556cb7b61a1cdec
ElliotHYLee <hylee101001@gmail.com>
2014-11-06 00:58:39 -0500

    desktop

--- C#/SerialMonitorTest01/SerialMonitorTest01/MainWindow.xaml.cs
+++ C#/SerialMonitorTest01/SerialMonitorTest01/MainWindow.xaml.cs
@@ -12,7 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO.Ports;
+using System.Threading;
 namespace SerialMonitorTest01
 {
     /// <summary>
@@ -20,40 +21,66 @@ namespace SerialMonitorTest01
     /// </summary>
     public partial class MainWindow : Window
     {
+        Boolean connectionStatus;
+        string[] ports;
+        SerialPort serial;
+        int numberOfPorts;
+        string recieved_data;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
-            txtComPort.Text = "";
-            txtBaudRate.Text = "";
-            txtSend.Text = "";
-            btnDisconnect.IsEnabled = false;
+            this.connectionStatus = false;
 
-        }
-                
-        private void btnSend(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("USB should be connected");
-            return;
-            
+            this.serial = new SerialPort();
+            this.ports = SerialPort.GetPortNames();
+            this.numberOfPorts = ports.Length;
+            for (int i = 0; i < numberOfPorts; i++)
+            {
+                this.comboPorts.Items.Insert(i, ports[i]);
+            }
+
+            this.txtBaudRate.Text = "115200";
+            this.txtSend.Text = "";
+            this.btnDisconnect.IsEnabled = false;
+            this.btnSend.IsEnabled = false;
+            this.lblStatus.Content = "No ports online.";
+            Console.WriteLine("Number of Ports Available: " + this.numberOfPorts);
         }
 
+   
         private void btnDisconnect_Clicked(object sender, RoutedEventArgs e)
         {
-            txtBaudRate.Text = "";
-            btnDisconnect.IsEnabled = false;
-            btnConnect.IsEnabled = true;
+            this.serial.Close();
+            this.connectionStatus = false;
+
+
+            this.txtBaudRate.Text = "";
+            this.btnDisconnect.IsEnabled = false;
+            this.btnConnect.IsEnabled = true;
+            this.btnSend.IsEnabled = false;
         }
 
         private void btnConnect_Clicked(object sender, RoutedEventArgs e)
         {
-            txtBaudRate.Text = "115200";
-            btnDisconnect.IsEnabled = true;
-            btnConnect.IsEnabled = false;
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
+            if (comboPorts.Text.Length > 0 && (txtBaudRate.Text.Equals("115200") || txtBaudRate.Text.Equals("9600")))
+            {
+                this.serial.PortName = comboPorts.Text;
+                this.serial.BaudRate = int.Parse(txtBaudRate.Text);
+                this.serial.Open();
+                this.btnDisconnect.IsEnabled = true;
+                this.btnConnect.IsEnabled = false;
+                this.connectionStatus = true;
+                this.btnSend.IsEnabled = true;
+                this.lblStatus.Content = serial.PortName + " online";
+            }
+            else
+            {
+                MessageBox.Show("Check your port or baud rate.");
+            }
+           
         }
 
         private void txtSend_TextChanged(object sender, TextChangedEventArgs e)
@@ -61,12 +88,53 @@ namespace SerialMonitorTest01
             
         }
 
+        private void comboPorts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txtBaudRate_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            this.comboPorts.Items.Clear();
+            this.ports = SerialPort.GetPortNames();
+            this.numberOfPorts = ports.Length;
+            for (int i = 0; i < numberOfPorts; i++)
+            {
+                this.comboPorts.Items.Insert(i, ports[i]);
+            }
+        }
+
         private void txtMonitor_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
 
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.connectionStatus)
+            {
 
+            }
+        }
+
+        #region Recieving
+
+        private delegate void UpdateUiTextDelegate(string text);
+        private void Recieve(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            // Collecting the characters received to our 'buffer' (string).
+            recieved_data = serial.ReadExisting();
+            txtMonitor.AppendText(recieved_data);
+            MessageBox.Show("ddd");
+
+        }
+        #endregion
+        
 
 
     }
